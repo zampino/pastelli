b3bf61db9ccf7b0c3c1500134507345b67f107d1
Andrea Amantini <lo.zampino@gmail.com>
2016-01-10 18:22:02 +0100

    adapted Elixir 1.2 new Keyword merge order

--- test/pastelli_test.exs
+++ test/pastelli_test.exs
@@ -6,10 +6,10 @@ defmodule PastelliTest do
   test "build arguments for elli:start_link" do
     assert build_elli_options(:http, __MODULE__, :plug_options,
       [some: :custom_options]) == [
-        some: :custom_options,
         callback_args: {__MODULE__, :plug_options},
         port: 4000,
         callback: Pastelli.Handler,
+        some: :custom_options,
       ]
   end
 end
