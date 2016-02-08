5a643e385d2c1ae29b358ddbd2157a5897d92f8c
Andrea Amantini <lo.zampino@gmail.com>
2016-02-08 18:27:28 +0100

    updating plug and poison deps; version bump at 0.2.4

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule Pastelli.Mixfile do
   use Mix.Project
-  @version "0.2.3"
+  @version "0.2.4"
 
   def project do
     [app: :pastelli,
@@ -21,7 +21,7 @@ defmodule Pastelli.Mixfile do
       {:plug, "~> 1.0"},
       {:elli, github: "knutin/elli"},
       {:elli_websocket, github: "mmzeeman/elli_websocket", compile: "make test compile"},
-      {:poison, "~> 1.3.0"},
+      {:poison, "~> 1.0"},
       {:hackney, "~> 1.3", only: :test},
       {:mock, "~> 0.1", only: :test}
     ]
